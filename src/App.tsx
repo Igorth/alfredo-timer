97035f87a640344dffba69948b178c2b00ebe329
Igorth <igordiasth@gmail.com>
2024-03-24 20:17:24 -0400

    feat: add styled components
    This commit add the styled components in the project, allowing us
    to manipulate our css using javascript in the separate files.
    
    We created the @types folder where we are overlaping the default
    theme for styled components, and use our global default theme.
    
    Doing a test using styled components in the Button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <div>
-      <h1>Timer</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
